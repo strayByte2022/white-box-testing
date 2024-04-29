1292e553b46d905805361fd3e5e3e8fc6710699f
l1nhx <linhhandsom@gmail.com>
2024-04-29 12:56:12 +0700

    test: add choose_course test

--- final-auto-test/init.py
+++ final-auto-test/init.py
@@ -2,7 +2,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 base_url = "https://school.moodledemo.net/login/index.php"
 username = "teacher"
 password = "moodle"
@@ -16,13 +17,29 @@ class InitTesting(unittest.TestCase):
         self.username = username
         self.password = password
 
-    def test_login(self):
+    def login(self):
         driver = self.driver
 
         username_box = driver.find_element(By.ID, "username")
         username_box.send_keys(self.username)
         password_box = driver.find_element(By.ID, "password")
-        password_box.send_keys(self.password+Keys.RETURN)
+        password_box.send_keys(self.password + Keys.RETURN)
+
+    def test_login(self):
+        self.login()
+
+    def choose_course(self):
+        self.login()
+        driver = self.driver
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "History: Russia in Revolution"))
+        )
+        course = driver.find_element(By.PARTIAL_LINK_TEXT,"History: Russia in Revolution")
+        course.click()
+
+    def test_choose_course(self):
+        self.choose_course()
+
     def tearDown(self):
         self.driver.close()
 
