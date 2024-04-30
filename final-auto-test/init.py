6ac4f4b2eea35997248da4daebee7e630a7220c3
l1nhx <linhhandsom@gmail.com>
2024-05-01 00:26:27 +0700

    test: add completion dialog test

--- final-auto-test/init.py
+++ final-auto-test/init.py
@@ -61,13 +61,51 @@ class InitTesting(unittest.TestCase):
         self.switch_button()
         driver = self.driver
         completion_button = driver.find_elements(By.XPATH, "//button[@data-toggle='dropdown']")
-        completion_button[1].click()
+        completion_button[2].click()
         choose_edit_button = driver.find_element(By.XPATH,"//a[@class='btn btn-sm px-2 py-0' and contains(@href, 'https://school.moodledemo.net/course/modedit.php?update=562&showonly=activitycompletionheader')]")
         choose_edit_button.click()
         time.sleep(2)
 
     def test_choose_edit_condition(self):
         self.choose_edit_condition()
+
+    def save_and_return(self):
+        driver = self.driver
+        save_button = driver.find_element(By.ID,"id_submitbutton2")
+        save_button.click()
+
+    def unlock_completion_setting(self):
+        self.choose_edit_condition()
+        driver = self.driver
+        unlock_button = driver.find_element(By.ID,"id_unlockcompletion")
+        unlock_button.click()
+
+    def test_case_no_completion_condition(self):
+        self.unlock_completion_setting()
+        driver = self.driver
+        none_button = driver.find_element(By.ID,"id_completion_0")
+        none_button.click()
+        self.save_and_return()
+
+    def case_stu_manually_mark_completion_no_reminder(self):
+        self.unlock_completion_setting()
+        driver = self.driver
+        manual_mark_button = driver.find_element(By.ID,"id_completion_1")
+        manual_mark_button.click()
+
+    def test_case_stu_manually_mark_completion_no_reminder(self):
+        self.case_stu_manually_mark_completion_no_reminder()
+        self.save_and_return()
+
+    def stu_manually_mark_completion_reminder_enable(self):
+        self.unlock_completion_setting()
+        driver = self.driver
+        enable_button = driver.find_element(By.ID,"id_completionexpected_enabled")
+        enable_button.click()
+
+    def test_case_stu_manually_mark_completion_reminder_normal_date(self):
+        self.stu_manually_mark_completion_reminder_enable()
+        self.save_and_return()
 
     def tearDown(self):
         self.driver.close()
